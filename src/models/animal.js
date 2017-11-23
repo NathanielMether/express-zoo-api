3a5d252a5357cc0f8859e2d5316e3bd6345e026b
NathanielMether <nmet3@student.monash.edu>
2017-11-23 11:56:26 +1100

    added find function to animal

--- src/models/animal.js
+++ src/models/animal.js
@@ -1,17 +1,21 @@
 let animals = [
   {
+    id: 1,
     name: 'Meercat',
     count: 27
   },
   {
+    id: 2,
     name: 'Lion',
     count: 4
   },
   {
+    id: 3,
     name: 'Black Rhino',
     count: 1
   },
   {
+    id: 4,
     name: 'Giraffe',
     count: 5
   }
@@ -21,6 +25,22 @@ function all() {
   return animals
 }
 
+function find(id) {
+  // insure 'id' is an integer
+  id = parseInt(id, 10)
+  let foundAnimal = null
+  // loop through all animals
+  animals.forEach((animal) => {
+    // compare this animals id to the one we want to find
+    if (animal.id === id) {
+      // If so, remember this animal
+      foundAnimal = animal
+    }
+  })
+  return foundAnimal
+}
+
 module.exports = {
-  all
+  all,
+  find
 }
--- src/routes/animals.js
+++ src/routes/animals.js
@@ -8,4 +8,16 @@ router.get('/animals', (req, res) => {
   res.json(animals)
 })
 
+router.get('/animals/:id', (req, res) => {
+  // find animal that matches id
+  const id = req.params.id
+  const animal = Animal.find(id)
+  if  (animal) {
+    res.json(animal)
+  }
+  else {
+    res.status(404).json({ error: `Animal with id '${id}' was not found` })
+  }
+})
+
 module.exports = router
